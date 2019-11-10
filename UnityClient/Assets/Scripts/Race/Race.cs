b70c0df0f40d477ef90167bf7b6f7112325addac
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-11-10 15:35:10 +0200

    Added base for race

---
+++ UnityClient/Assets/Scripts/Race/Race.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSMOS.Race
+{
+    public class Race
+    {
+        public static IReadOnlyList<Race> Races
+        {
+            get
+            {
+                return races.AsReadOnly();
+            }
+        }
+        static List<Race> races = new List<Race>();
+
+        public string Name { get; protected set; }
+        public Dictionary<Race, float> relations = new Dictionary<Race, float>();
+    }
+}
