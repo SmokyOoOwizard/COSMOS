19aeebc660dff27f3ac8ba8b06dc495192737a59
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-11-10 14:33:06 +0200

    Added base for ability and skills

---
+++ UnityClient/Assets/Scripts/Skills/Abilities.cs
@@ -0,0 +1,11 @@
+﻿using COSMOS.Skills.Ability;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace COSMOS
+{
+    public class Abilities
+    {
+        List<Ability> abilities = new List<Ability>();
+    }
+}
---
+++ UnityClient/Assets/Scripts/Skills/Ability/Ability.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSMOS.Skills.Ability
+{
+    public abstract class Ability
+    {
+        public string inerName;
+        public string Name;
+        public uint CurrentXP;
+        public uint CurrentLevel;
+        public List<uint> Levels = new List<uint>();
+
+        public void AddXP(uint xp)
+        {
+            uint needXpToNextLevel = Levels[(int)CurrentLevel];
+            CurrentXP += xp;
+            if(CurrentXP >= needXpToNextLevel)
+            {
+                CurrentXP -= needXpToNextLevel;
+
+                nextLevel();
+            }
+        }
+
+        void nextLevel()
+        {
+            if (CurrentLevel < Levels.Count - 1)
+            {
+                CurrentLevel++;
+                levelUp();
+            }
+        }
+        protected abstract void levelUp();
+    }
+    public abstract class SubAbility : Ability
+    {
+
+    }
+    public abstract class MainAblity : Ability
+    {
+        public List<SubAbility> SubAbilities = new List<SubAbility>();
+
+    }
+}
