10337b04953f262859d1bfbfe8d8ff54af086a32
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-11-13 23:21:50 +0200

    Added stub for SpaceShipController

---
+++ UnityClient/Assets/Scripts/SpaceShip/SpaceShipController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using COSMOS.Player;
+using UnityEngine;
+namespace COSMOS.SpaceShip
+{
+    public class SpaceShipController : MonoBehaviour, IControllable
+    {
+        public bool Exist()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vector3 GetPos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(Vector2 dir)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rotate(float angle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Selected()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
