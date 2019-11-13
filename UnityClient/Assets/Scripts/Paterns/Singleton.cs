0452019f37e85572a8384de3f9562b1c506ca7fd
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-11-13 22:43:07 +0200

    Added patern singleton

---
+++ UnityClient/Assets/Scripts/Paterns/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace COSMOS.Paterns
+{
+    public abstract class SingletonMono<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        public static T instance;
+        public void InitFather()
+        {
+            if (!instance)
+            {
+                instance = this as T;
+                GameObject.DontDestroyOnLoad(instance.gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+    public abstract class Singleton<T> where T : class
+    {
+        public static T instance;
+        public Singleton(T obj)
+        {
+            instance = obj;
+        }
+    }
+}
