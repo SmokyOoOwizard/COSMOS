d7f8fd02ebf4098f49377af7ae066862966e6e9d
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-11-17 15:05:55 +0200

    added SpaceShipHull and SpaceShipController

--- UnityClient/Assets/Scripts/SpaceShip/SpaceShipController.cs
+++ UnityClient/Assets/Scripts/SpaceShip/SpaceShipController.cs
@@ -3,35 +3,184 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using COSMOS.HelpfullStuff;
 using COSMOS.Player;
 using UnityEngine;
+
 namespace COSMOS.SpaceShip
 {
+    [RequireComponent(typeof(Rigidbody))]
     public class SpaceShipController : MonoBehaviour, IControllable
     {
+        public bool Enable;
+        public bool EnableInputControl;
+        public float TargetRotation;
+        public Vector3 torque;
+
+        SpaceShipHull Hull;
+        new Rigidbody rigidbody;
+        PID AngleCont, VelocityCont;
+        private void Awake()
+        {
+            rigidbody = GetComponent<Rigidbody>();
+            CreateDebugShip();
+
+            AngleCont = new PID();
+            AngleCont.Kp = 9.244681f;
+            AngleCont.Ki = 0;
+            AngleCont.Kd = 0.06382979f;
+
+            VelocityCont = new PID();
+            VelocityCont.Kp = 33.7766f;
+            VelocityCont.Ki = 0;
+            VelocityCont.Kd = 0.2553191f;
+        }
+
         public bool Exist()
         {
-            throw new NotImplementedException();
+            return this != null;
         }
 
         public Vector3 GetPos()
         {
-            throw new NotImplementedException();
+            return transform.position;
         }
 
         public void Move(Vector2 dir)
         {
-            throw new NotImplementedException();
+            if (Enable)
+            {
+                if (rigidbody.velocity.magnitude > Hull.MaxSpeed)
+                {
+                    rigidbody.AddForce(-rigidbody.velocity, ForceMode.Force);
+                    return;
+                }
+                if (EnableInputControl)
+                {
+                    UseForwardEngine(Mathf.Clamp(dir.y, 0, 1));
+                    UseBrakingEngine(Mathf.Clamp(dir.y, -1, 0));
+                    UseSideEngine(Mathf.Clamp(dir.x, -1, 1));
+                }
+            }
         }
 
         public void Rotate(float angle)
         {
-            throw new NotImplementedException();
+            if (Enable && EnableInputControl)
+            {
+                TargetRotation = angle;
+                if (TargetRotation > 180)
+                {
+                    TargetRotation -= 360;
+                }
+            }
         }
 
         public void Selected()
         {
-            throw new NotImplementedException();
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Brake();
+            }
+        }
+        public void Brake()
+        {
+            if (EnableInputControl)
+            {
+                rigidbody.AddTorque(-rigidbody.angularVelocity * 0.9f, ForceMode.Force);
+                Vector3 local = transform.InverseTransformVector(rigidbody.velocity);
+                //
+                UseForwardEngine(Mathf.Abs(Mathf.Clamp((local.z / Hull.MainEngine.MaxForce), -1, 0)));
+                UseBrakingEngine(Mathf.Abs(Mathf.Clamp((local.z / Hull.BrakingEngine.MaxForce), 0, 1)));
+                UseSideEngine(Mathf.Clamp((-local.x / Hull.SideEngines.MaxForce), -1, 1));
+            }
+        }
+        private void FixedUpdate()
+        {
+            if (Enable)
+            {
+                if (EnableInputControl)
+                {
+                    TurnUpdate();
+                }
+            }
+        }
+        #region Engines
+        public void UseForwardEngine(float power)
+        {
+            float f = Hull.UseForwardEngine(Mathf.Abs(power));
+            if(f > 0)
+            {
+                rigidbody.AddForce(transform.forward * f, ForceMode.Force);
+            }
+        }
+        public void UseBrakingEngine(float power)
+        {
+            float f = Hull.UseBrakingEngine(Mathf.Abs(power));
+            if (f > 0)
+            {
+                rigidbody.AddForce(transform.forward * f * -1, ForceMode.Force);
+            }
+        }
+        public void UseSideEngine(float power)
+        {
+            float f = Hull.UseSideEngine(Mathf.Abs(power));
+            if(f > 0)
+            {
+                rigidbody.AddForce(transform.right * Mathf.Sign(power) * f, ForceMode.Force);
+            }
+        }
+        private void TurnUpdate()
+        {
+            float target = (Quaternion.Inverse(transform.rotation) * Quaternion.Euler(0, TargetRotation, 0)).eulerAngles.y;
+            if (target > 180)
+            {
+                target -= 360;
+            }
+            float dt = Time.fixedDeltaTime;
+
+            // The target angle is the user-driven angle that the ship will be (hopefully) 
+            // steered towards. The target angle is represented in Unity's scene view by a 
+            // white line.
+            float targetAngle = transform.eulerAngles.y + (Mathf.Clamp(target, -1, 1)) * Hull.TurnEngines.MaxForce * dt;
+            //targetAngle = TargetY;// * MaxRotationSpeed * dt;
+
+            // The angle controller drives the ship's angle towards the target angle.
+            // This PID controller takes in the error between the ship's rotation angle 
+            // and the target angle as input, and returns a signed torque magnitude.
+            float angleError = Mathf.DeltaAngle(transform.eulerAngles.y, targetAngle);
+            float torqueCorrectionForAngle = AngleCont.GetOutput(angleError, dt);
+
+            // The angular veloicty controller drives the ship's angular velocity to 0.
+            // This PID controller takes in the negated angular velocity of the ship, 
+            // and returns a signed torque magnitude.
+            float angularVelocityError = -rigidbody.angularVelocity.y;
+            float torqueCorrectionForAngularVelocity = VelocityCont.GetOutput(angularVelocityError, dt);
+
+            // The total torque from both controllers is applied to the ship. If we've got 
+            // our gains right, then this force will correctly steer the ship to the target 
+            // angle and try to hold it there. The torque vector is represented in Unity's 
+            // scene view by a red line.
+            torque = transform.up * (torqueCorrectionForAngle + torqueCorrectionForAngularVelocity);
+
+            float f = (float)Math.Round(Mathf.Clamp01(Mathf.Abs(torqueCorrectionForAngle / 10)), 2);
+            //Debug.Log((float)Math.Round(Mathf.Clamp01(Mathf.Abs(torque.y / 10)) * Hull.SteeringEngines.FuelConsumption, 2));
+            if (Hull.UseTurnEngine(f) > 0)
+            {
+                //GetComponent<Rigidbody>().AddRelativeTorque(torque);
+                rigidbody.AddTorque(torque);
+            }
+        }
+        #endregion
+        public void CreateDebugShip()
+        {
+            Hull = new SpaceShipHull();
+            Hull.ReplaceEngine(new TurnEngine(100, "fuel", 1));
+            Hull.ReplaceEngine(new SideEngine(20, "fuel", 1));
+            Hull.ReplaceEngine(new MainEngine(20, "fuel", 1));
+            Hull.ReplaceEngine(new BrakingEngine(20, "fuel", 1));
+
+            Hull.AddTank(new Tank("fuel", 10, 10));
         }
     }
 }
---
+++ UnityClient/Assets/Scripts/SpaceShip/SpaceShipHull.cs
@@ -0,0 +1,162 @@
+﻿using COSMOS.Equipment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+namespace COSMOS.SpaceShip
+{
+    public class SpaceShipHull
+    {
+        public float HullWeight { get; protected set; }
+        public float TotalWeight { get; protected set; }
+        public float MaxWeight { get; protected set; }
+
+        public float FreeVolume { get; protected set; }
+        public float MaxVolume { get; protected set; }
+
+        public float MaxSpeed { get; protected set; } = 20;
+
+        #region Equipments
+        public MainEngine MainEngine { get; protected set; }
+        public BrakingEngine BrakingEngine { get; protected set; }
+        public TurnEngine TurnEngines { get; protected set; }
+        public SideEngine SideEngines { get; protected set; }
+
+        public List<Tank> Tanks { get; protected set; }
+        #endregion
+
+        public Engine ReplaceEngine(Engine engine)
+        {
+            if(engine is MainEngine)
+            {
+                Engine tmp = MainEngine;
+                MainEngine = engine as MainEngine;
+                return tmp;
+            }
+            else if(engine is BrakingEngine)
+            {
+                Engine tmp = BrakingEngine;
+                BrakingEngine = engine as BrakingEngine;
+                return tmp;
+            }
+            else if(engine is TurnEngine)
+            {
+                Engine tmp = TurnEngines;
+                TurnEngines = engine as TurnEngine;
+                return tmp;
+            }
+            else if(engine is SideEngine)
+            {
+                Engine tmp = SideEngines;
+                SideEngines = engine as SideEngine;
+                return tmp;
+            }
+            return null;
+        }
+        public void AddTank(Tank tank)
+        {
+            if (Tanks == null)
+            {
+                Tanks = new List<Tank>();
+            }
+            Tanks.Add(tank);
+        }
+        public float UseFuel(float amount, string fuelType)
+        {
+            Tank tank = Tanks.Find((x) => { return x.FuelType == fuelType; });
+            if(tank != null)
+            {
+                return tank.Drain(amount);
+            }
+            return 0;
+        }
+        #region Engines
+        public float CalculateForwardEngineForce(float power)
+        {
+            if(MainEngine != null)
+            {
+                return MainEngine.CalculateEngineForce(power);
+            }
+            return 0;
+        }
+        public float UseForwardEngine(float power)
+        {
+            if (MainEngine != null)
+            {
+                float fuelConsumption = power * MainEngine.FuelConsumption;
+                float fuelUsedUp = UseFuel(fuelConsumption, MainEngine.FuelType);
+                if (fuelUsedUp > 0)
+                {
+                    return MainEngine.CalculateEngineForce(power * (fuelUsedUp / fuelConsumption));
+                }
+            }
+            return 0;
+        }
+        public float CalculateBrakingEngineForce(float power)
+        {
+            if (BrakingEngine != null)
+            {
+                return BrakingEngine.CalculateEngineForce(power);
+            }
+            return 0;
+        }
+        public float UseBrakingEngine(float power)
+        {
+            if (BrakingEngine != null)
+            {
+                float fuelConsumption = power * BrakingEngine.FuelConsumption;
+                float fuelUsedUp = UseFuel(fuelConsumption, BrakingEngine.FuelType);
+                if (fuelUsedUp > 0)
+                {
+                    return BrakingEngine.CalculateEngineForce(power * (fuelUsedUp / fuelConsumption));
+                }
+            }
+            return 0;
+        }
+        public float CalculateTurnEngineForce(float power)
+        {
+            if (TurnEngines != null)
+            {
+                return TurnEngines.CalculateEngineForce(power);
+            }
+            return 0;
+        }
+        public float UseTurnEngine(float power)
+        {
+            if (TurnEngines != null)
+            {
+                float fuelConsumption = power * TurnEngines.FuelConsumption;
+                float fuelUsedUp = UseFuel(fuelConsumption, TurnEngines.FuelType);
+                if (fuelUsedUp > 0)
+                {
+                    return TurnEngines.CalculateEngineForce(power * (fuelUsedUp / fuelConsumption));
+                }
+            }
+            return 0;
+        }
+        public float CalculateSideEngineForce(float power)
+        {
+            if (SideEngines != null)
+            {
+                return SideEngines.CalculateEngineForce(power);
+            }
+            return 0;
+        }
+        public float UseSideEngine(float power)
+        {
+            if (SideEngines != null)
+            {
+                float fuelConsumption = power * SideEngines.FuelConsumption;
+                float fuelUsedUp = UseFuel(fuelConsumption, SideEngines.FuelType);
+                if (fuelUsedUp > 0)
+                {
+                    return SideEngines.CalculateEngineForce(power * (fuelUsedUp / fuelConsumption));
+                }
+            }
+            return 0;
+        }
+        #endregion
+    }
+}
