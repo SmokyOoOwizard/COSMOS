5b13620034414407004889db4fa14e7651afeaa9
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-11-17 15:04:09 +0200

    Added base Equipment for spaceship

---
+++ UnityClient/Assets/Scripts/Equipment/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSMOS.Equipment
+{
+    public class Item
+    {
+        public float Weight { get; protected set; }
+        public float Volume { get; protected set; }
+    }
+}
---
+++ UnityClient/Assets/Scripts/SpaceShip/Tank.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using COSMOS.Equipment;
+
+namespace COSMOS.SpaceShip
+{
+    public class Tank : Item
+    {
+        public string FuelType { get; protected set; }
+        public float FuelVolume { get; protected set; }
+        public float MaxFuelVolume { get; protected set; }
+
+        public Tank(string fuelType, float maxFuelVolume, float fuelVolume)
+        {
+            FuelType = fuelType;
+            FuelVolume = fuelVolume;
+            MaxFuelVolume = maxFuelVolume;
+        }
+
+        public float Drain(float amount)
+        {
+            if (FuelVolume - amount > 0)
+            {
+                //FuelVolume -= amount;
+                return amount;
+            }
+            else
+            {
+                float d = (FuelVolume - amount) + amount;
+                //FuelVolume -= d;
+                return d;
+            }
+        }
+    }
+}
