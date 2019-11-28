fe1403b2b5e662566c337af2e6f57edc466c112e
tsar <tsarstvennyy@luxurygames.ch>
2019-11-28 11:27:21 +0200

    breaking class SpaceShipHull on logic segments and added new stat ShipEquipment

--- UnityClient/Assets/Scripts/Equipment/ShipEquipment.cs
+++ UnityClient/Assets/Scripts/Equipment/ShipEquipment.cs
@@ -9,5 +9,6 @@ namespace COSMOS.Equipment
     public abstract class ShipEquipment : Item
     {
         public float Durability { get; protected set; }
+        public float RateWear { get; protected set; }
     }
 }
---
+++ UnityClient/Assets/Scripts/SpaceShip/SpaceShipHull_Equipments.cs
@@ -0,0 +1,130 @@
+﻿using COSMOS.Equipment;
+using COSMOS.SpaceShip.Equipment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSMOS.SpaceShip
+{
+    public partial class SpaceShipHull
+    {
+        public List<ShipEquipment> Equipments { get; protected set; }
+        public MainEngine MainEngine { get; protected set; }
+        public BrakingEngine BrakingEngine { get; protected set; }
+        public TurnEngine TurnEngines { get; protected set; }
+        public SideEngine SideEngines { get; protected set; }
+        public WarpEngine WarpEngine { get; protected set; }
+        public List<Tank> Tanks { get; protected set; }
+        public List<Generator> Generators { get; protected set; }
+        public List<EnergyTank> EnergyTanks { get; protected set; }
+        public List<INeedEnergy> EnergyСonsumptions { get; protected set; }
+
+        public void InitEquipments()
+        {
+
+        }
+
+        public Engine ReplaceEngine(Engine engine)
+        {
+            if (engine is MainEngine)
+            {
+                Engine tmp = MainEngine;
+                MainEngine = engine as MainEngine;
+                return tmp;
+            }
+            else if (engine is BrakingEngine)
+            {
+                Engine tmp = BrakingEngine;
+                BrakingEngine = engine as BrakingEngine;
+                return tmp;
+            }
+            else if (engine is TurnEngine)
+            {
+                Engine tmp = TurnEngines;
+                TurnEngines = engine as TurnEngine;
+                return tmp;
+            }
+            else if (engine is SideEngine)
+            {
+                Engine tmp = SideEngines;
+                SideEngines = engine as SideEngine;
+                return tmp;
+            }
+            return null;
+        }
+        public void AddEquipment(ShipEquipment equipment)
+        {
+            switch (equipment)
+            {
+                case Tank tank:
+                    AddTank(tank);
+                    break;
+                case Generator g:
+                    AddGenerator(g);
+                    break;
+            }
+
+            if (equipment is INeedEnergy)
+            {
+                EnergyСonsumptions.Add(equipment as INeedEnergy);
+            }
+        }
+        public void RemoveEquipment(ShipEquipment equipment)
+        {
+            switch (equipment)
+            {
+                case Tank tank:
+                    RemoveTank(tank);
+                    break;
+                case Generator g:
+                    RemoveGenerator(g);
+                    break;
+            }
+
+            if (equipment is INeedEnergy)
+            {
+                EnergyСonsumptions.Remove(equipment as INeedEnergy);
+            }
+        }
+        void AddGenerator(Generator generator)
+        {
+            Equipments.Add(generator);
+            Generators.Add(generator);
+        }
+        void RemoveGenerator(Generator generator)
+        {
+            Equipments.Remove(generator);
+            Generators.Remove(generator);
+        }
+        void AddTank(Tank tank)
+        {
+            Equipments.Add(tank);
+            Tanks.Add(tank);
+        }
+        void RemoveTank(Tank tank)
+        {
+            Equipments.Remove(tank);
+            Tanks.Remove(tank);
+        }
+        public float GetFuelCount(string fuelType)
+        {
+            Tank tank = Tanks.Find((x) => { return x.FuelType == fuelType; });
+            if (tank != null)
+            {
+                return tank.FuelVolume;
+            }
+            return 0;
+        }
+        public float UseFuel(float amount, string fuelType)
+        {
+            Tank tank = Tanks.Find((x) => { return x.FuelType == fuelType; });
+            if (tank != null)
+            {
+                return tank.Drain(amount);
+            }
+            return 0;
+        }
+    }
+}
