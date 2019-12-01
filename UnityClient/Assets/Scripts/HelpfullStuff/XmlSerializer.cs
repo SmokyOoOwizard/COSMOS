2ebc07fa771372a9df7bf6718bc88e54f3da4d6b
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-12-01 14:19:45 +0200

    added XmlSerializer helper

---
+++ UnityClient/Assets/Scripts/HelpfullStuff/XmlSerializer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSMOS.HelpfullStuff
+{
+    public static class XmlSerializer
+    {
+        public static T Deserialize<T>(string xml)
+        {
+            System.Xml.Serialization.XmlSerializer xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+            T result;
+
+            using (TextReader reader = new StringReader(xml))
+            {
+                result = (T)xmlSerializer.Deserialize(reader);
+            }
+
+            return result;
+        }
+        public static string Serialize<T>(T obj)
+        {
+            System.Xml.Serialization.XmlSerializer xmlSerializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+            using (StringWriter textWriter = new StringWriter())
+            {
+                xmlSerializer.Serialize(textWriter, obj);
+                return textWriter.ToString();
+            }
+        }
+    }
+}
