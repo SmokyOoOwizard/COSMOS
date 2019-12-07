bffe870b4a95c706f732974eeb124ee2e8ba8f9c
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-12-07 23:16:11 +0200

    added planet prototype

--- UnityClient/Assets/Scripts/Space/PlanetProto.cs
+++ UnityClient/Assets/Scripts/Space/PlanetProto.cs
@@ -9,10 +9,14 @@ using System.Xml.Serialization;
 using COSMOS.Relations;
 namespace COSMOS.Space
 {
+    [BindProto("Planet")]
     public class PlanetProto : SpaceObject
     {
+        [BindProto(true)]
         public float Size;
+        [BindProto(true)]
         public float OrbitSpeed;
+        [BindProto(true)]
         public float OrbitSize;
         public override Fraction GetFraction()
         {
