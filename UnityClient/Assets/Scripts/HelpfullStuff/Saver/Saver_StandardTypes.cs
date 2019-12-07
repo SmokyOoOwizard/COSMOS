c6fc4739f34380da3bb900068dd9f5fb3a5315a3
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-12-07 14:10:37 +0200

    Added methods for save types to string

---
+++ UnityClient/Assets/Scripts/HelpfullStuff/Saver/Saver_StandardTypes.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COSMOS.HelpfullStuff
+{
+    public static partial class Saver
+    {
+        public static string SaveInt(long raw)
+        {
+            return raw.ToString();
+        }
+        public static string SaveInt(long? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.ToString();
+            }
+            return null;
+        }
+        public static string SaveUInt(ulong raw)
+        {
+            return raw.ToString();
+        }
+        public static string SaveUInt(ulong? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.ToString();
+            }
+            return null;
+        }
+        public static string SaveChar(char raw)
+        {
+            return raw.ToString();
+        }
+        public static string SaveChar(char? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.ToString();
+            }
+            return null;
+        }
+        public static string SaveFloat(float raw)
+        {
+            return raw.ToString();
+        }
+        public static string SaveFloat(float? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.ToString();
+            }
+            return null;
+        }
+        public static string SaveDouble(double raw)
+        {
+            return raw.ToString();
+        }
+        public static string SaveDouble(double? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.ToString();
+            }
+            return null;
+        }
+        public static string SaveDecimal(decimal raw)
+        {
+            return raw.ToString();
+        }
+        public static string SaveDecimal(decimal? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.ToString();
+            }
+            return null;
+        }
+        public static string SaveBool(bool raw)
+        {
+            return raw.ToString();
+        }
+        public static string SaveBool(bool? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.ToString();
+            }
+            return null;
+        }
+        public static string SaveEnum(Enum @enum)
+        {
+            return @enum.ToString();
+        }
+    }
+}
---
+++ UnityClient/Assets/Scripts/HelpfullStuff/Saver/Saver_UnityTypes.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace COSMOS.HelpfullStuff
+{
+    public static partial class Saver
+    {
+        public static string SaveVector2(Vector2 raw)
+        {
+            return raw.x + ";" + raw.y;
+        }
+        public static string SaveVector2(Vector2? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.Value.x + ";" + raw.Value.y;
+            }
+            return null;
+        }
+        public static string SaveVector2Int(Vector2Int raw)
+        {
+            return raw.x + ";" + raw.y;
+        }
+        public static string SaveVector2Int(Vector2Int? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.Value.x + ";" + raw.Value.y;
+            }
+            return null;
+        }
+        public static string SaveVector3(Vector3 raw)
+        {
+            return raw.x + ";" + raw.y + ";" + raw.z;
+        }
+        public static string SaveVector3(Vector3? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.Value.x + ";" + raw.Value.y + ";" + raw.Value.z;
+            }
+            return null;
+        }
+        public static string SaveVector3Int(Vector3Int raw)
+        {
+            return raw.x + ";" + raw.y + ";" + raw.z;
+        }
+        public static string SaveVector3Int(Vector3Int? raw)
+        {
+            if (raw.HasValue) 
+            {
+                return raw.Value.x + ";" + raw.Value.y + ";" + raw.Value.z;
+            }
+            return null;
+        }
+        public static string SaveVector4(Vector4 raw)
+        {
+            return raw.x + ";" + raw.y + ";" + raw.z + ";" + raw.w;
+        }
+        public static string SaveVector4(Vector4? raw)
+        {
+            if (raw.HasValue)
+            {
+                return raw.Value.x + ";" + raw.Value.y + ";" + raw.Value.z + ";" + raw.Value.w;
+            }
+            return null;
+        }
+        public static string SaveQuaternion(Quaternion raw)
+        {
+#warning NOT COMPLETE
+            return "";
+        }
+        public static string SaveQuaternion(Quaternion? raw)
+        {
+#warning NOT COMPLETE
+            return "";
+        }
+        public static string SaveRect(Rect raw)
+        {
+#warning NOT COMPLETE
+            return "";
+        }
+        public static string SaveRect(Rect? raw)
+        {
+#warning NOT COMPLETE
+            return "";
+        }
+        public static string SaveColor(Color raw)
+        {
+            string r = ((int)(raw.r * 255)).ToString("X");
+            string g = ((int)(raw.g * 255)).ToString("X");
+            string b = ((int)(raw.b * 255)).ToString("X");
+            string a = ((int)(raw.a * 255)).ToString("X");
+            return r + g + b + a;
+        }
+        public static string SaveColor(Color? raw)
+        {
+            if (raw.HasValue) 
+            {
+                string r = ((int)(raw.Value.r * 255)).ToString("X");
+                string g = ((int)(raw.Value.g * 255)).ToString("X");
+                string b = ((int)(raw.Value.b * 255)).ToString("X");
+                string a = ((int)(raw.Value.a * 255)).ToString("X");
+                return r + g + b + a;
+            } 
+            return null;
+        }
+        public static string SaveRectOffset(RectOffset raw)
+        {
+#warning NOT COMPLETE
+            return "";
+        }
+    }
+}
